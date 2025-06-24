82e300854be0c17c2869fdc3d37ec312d7ad23b0
Rcontre360 <rcontreraspimentel@gmail.com>
2025-06-23 22:24:07 -0400

    feat: concept for lossfn and optimizer

--- tarea1/experiment/rafael_nn/nn.py
+++ tarea1/experiment/rafael_nn/nn.py
@@ -1,21 +1,48 @@
 import numpy as np
+from numpy.typing import NDArray
 
 from rafael_nn.layer import Linear
+from rafael_nn.lossfn import LossFunction
 from rafael_nn.optimizer import Optimizer
 
 class NeuralNetwork:
-    def __init__(self, layers:list[Linear], optimizer:Optimizer):
+    # I like adding types. Its easier to know that what im doing will work, also easier to debug
+    # want to update this to use functional programming
+    layers: list[Linear]
+    optimizer:Optimizer
+    layers_output: list[NDArray[np.float64]]
+    loss_fn:LossFunction
+
+    def __init__(self, layers:list[Linear], optimizer:Optimizer, loss_fn:LossFunction):
+        self.layers_output = [] * len(layers)
         self.layers = layers
         self.optimizer = optimizer
+        self.loss_fn = loss_fn
+
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        return self._forward(x)
+
+    def train(self, x: np.ndarray):
+        # here we determine WHEN to stop but the optimizer tells us the next step
+        self._forward(x, True)
+        gradients = self._backward()
+        self.weights = self.optimizer(self.weights, gradients)
 
     def forward(self, x: np.ndarray) -> np.ndarray:
+        return self._forward(x)
+
+    def _forward(self, x: np.ndarray, train=False) -> np.ndarray:
         current = x
-        for layer in self.layers:
+        for i in range(len(self.layers)):
+            layer = self.layers[i]
             current = layer.forward(current)
+            if train:
+                self.layers_output[i] = current
 
         return current
 
-    def backward(self, loss_grad: np.ndarray) -> None:
+    def _backward(self) -> None:
+        # here the loss fn is used
         pass
 
     def update(self, optimizer: Optimizer) -> None:
--- tarea1/experiment/rafael_nn/optimizer.py
+++ tarea1/experiment/rafael_nn/optimizer.py
@@ -3,6 +3,14 @@ from abc import ABC, abstractmethod
 
 class Optimizer(ABC):
     @abstractmethod
-    def update(self, weights: np.ndarray, gradients: np.ndarray, layer_index: int) -> np.ndarray:
+    def __call__(self, weights: np.ndarray, gradients: np.ndarray, layer_index: int) -> np.ndarray:
         pass
+
+class GradientDescent(Optimizer):
+    def __init__(self, learning_rate: float = 0.01):
+        self.learning_rate = learning_rate
+
+    def __call__(self, parameters: list[np.ndarray], gradients: list[np.ndarray]) -> None:
+        for param, grad in zip(parameters, gradients):
+            param -= self.learning_rate * grad
 
