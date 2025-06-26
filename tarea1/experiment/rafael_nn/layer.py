fix: fixed bias derivative calc

tarea1/experiment/rafael_nn/layer.py
@@ -40,7 +40,8 @@ class Linear(Layer):
         """Initializes linear layer with weights. Initializes biases with 0"""
         self.fn = ReLU(neurons) if fn is None else fn
         weights = [[self.fn.init_sample() for _ in range(prev)] for _ in range(neurons)]
-        biases = [0 for _ in range(neurons)]
+        # TODO, output is 0 if bias is 0. need to check this
+        biases = [self.fn.init_sample() for _ in range(neurons)]
 
         self.prev = prev
         self.neurons = neurons
@@ -67,7 +68,8 @@ class Linear(Layer):
             dl_f = prev_dl_f
 
         # dl_bias, dl_f and dl_weight
-        return dl_f, dl_f, np.matmul(dl_f, self.h.T)
+        print("shapes",dl_f.shape, self.h.shape)
+        return dl_f, dl_f, dl_f@self.h.T
 
     def __str__(self):
         return f"Linear({self.prev},{self.neurons})"

tarea1/experiment/rafael_nn/nn.py
@@ -6,6 +6,8 @@ from rafael_nn.layer import Layer
 from rafael_nn.lossfn import LossFunction
 from rafael_nn.optimizer import Optimizer
 
+np.random.seed(42)
+
 class NeuralNetwork:
     # I like adding types. Its easier to know that what im doing will work, also easier to debug
     # want to update this to use functional programming
@@ -30,8 +32,11 @@ class NeuralNetwork:
         if loss < err:
             return
 
-        _,_,dl_w = self._backward(final, target)
+        _,dl_w = self._backward(final, target)
         # self.optimizer(self.weights, dl_w)
+
+    def backward(self, prediction:FloatArr, target:FloatArr) -> tuple[list[FloatArr],list[FloatArr]]:
+        return self._backward(prediction,target)
 
     # this is almos the same implementation as the 7_2 notebook
     def _forward(self, x: np.ndarray) -> tuple[FloatArr, list[FloatArr], list[FloatArr]]:
@@ -45,15 +50,22 @@ class NeuralNetwork:
 
         return all_h[-1], all_h, all_f
 
-    def _backward(self, prediction:FloatArr, target:FloatArr) -> tuple[FloatArr,FloatArr,FloatArr]:
-        dl_b, prev_dl_f, dl_w = self.layers[-1].backward(prev_dl_f=self.loss_fn.backward(prediction,target))
+    def _backward(self, prediction:FloatArr, target:FloatArr) -> tuple[list[FloatArr],list[FloatArr]]:
+        layers_n = len(self.layers)
+        all_dl_bias, all_dl_weights = [0] * layers_n, [0] * layers_n
 
-        for i in range(len(self.layers) - 2,-1,-1):
+        dl_b, prev_dl_f, dl_w = self.layers[-1].backward(prev_dl_f=self.loss_fn.backward(prediction,target))
+        all_dl_bias[-1] = dl_b
+        all_dl_weights[-1] = dl_w
+
+        for i in range(layers_n - 2,-1,-1):
             # here had an anoying error because prev_w was already transposed on this line
             prev_w = self.layers[i+1].weights
-            dl_b, prev_dl_f, dl_w = self.layers[i].backward(prev_w.T @ prev_dl_f)
+            dl_b, prev_dl_f, dl_w = self.layers[i].backward(weights_dl_f=prev_w.T @ prev_dl_f)
+            all_dl_bias[i] = dl_b
+            all_dl_weights[i] = dl_w
 
-        return dl_b, prev_dl_f, dl_w
+        return all_dl_bias, all_dl_weights
 
     def update(self, optimizer: Optimizer) -> None:
         pass

tarea1/experiment/tests/test_nn_backprop.py
@@ -12,38 +12,78 @@ from rafael_nn.optimizer import GradientDescent
 class TestNNGradient(unittest.TestCase):
     def setUp(self):
         np.random.seed(42)
-        layers = 3
-        n_by_layer = 5
-        layers:list[Layer] = [Linear(1,n_by_layer)] + [Linear(n_by_layer,n_by_layer) for _ in range(layers)] + [Linear(n_by_layer,1)]
+        layers = 5
+        n_by_layer = 6
+        layers:list[Layer] = [Linear(1,n_by_layer)] + [Linear(n_by_layer,n_by_layer) for _ in range(layers-1)] + [Linear(n_by_layer,1)]
 
-        self.nn = NeuralNetwork(layers, optimizer=GradientDescent(), loss_fn=MeanSquaredError())
+        self.loss_fn = MeanSquaredError()
+        self.nn = NeuralNetwork(layers, optimizer=GradientDescent(), loss_fn=self.loss_fn)
 
-    def test_nn_gradient_calc(self):
-        x = np.random.randn(1)
+    # def test_nn_gradient_with_bias_change(self):
+    def aux(self):
+        """Here we perform the same kind of check made in the notebook 7_2 to see if our derivatives are well calculated"""
+        # x = np.random.randn(1)
+        x = np.array([1.2])
         target = np.array([1.0])
-        epsilon = 1e-5
-        tolerance = 1e-4
+        epsilon = 0.000001 # same delta used in notebook 7_2
 
-        prediction, _, _ = self.nn._forward(x)
-        dx_analytical, _, _ = self.nn._backward(prediction, target)
+        prediction = self.nn(x)
+        all_dl_b, _ = self.nn._backward(prediction, target)
 
-        dx_numerical = np.zeros_like(x)
-        for i in range(x.size):
-            x_pos = x.copy()
-            x_neg = x.copy()
-            x_pos[i] += epsilon
-            x_neg[i] -= epsilon
+        for layer_i in range(len(self.nn.layers)):
+            layer = self.nn.layers[layer_i]
 
-            fx_pos, _, _ = self.nn._forward(x_pos)
-            fx_neg, _, _ = self.nn._forward(x_neg)
+            bias_cpy = np.array(layer.biases)
+            dl_bias = np.zeros_like(all_dl_b[layer_i])
 
-            loss_pos = self.nn.loss_fn(fx_pos, target)
-            loss_neg = self.nn.loss_fn(fx_neg, target)
+            for row in range(len(layer.biases)):
+                # get current output
+                output = self.nn(x)
+                layer.biases[row]+=epsilon
+                # get output with small change
+                output_change = self.nn(x)
 
-            dx_numerical[i] = (loss_pos - loss_neg) / (2 * epsilon)
+                #restore previous value
+                layer.biases = bias_cpy
+                dl_bias[row] = (self.loss_fn(output_change,target) - self.loss_fn(output, target))/epsilon
 
-        # === Compare ===
-        assert np.allclose(dx_analytical, dx_numerical, atol=tolerance)
-        print("Gradient check passed!")
-        pass
+            assert np.allclose(all_dl_b[layer_i], dl_bias, rtol=1e-5, atol=1e-8, equal_nan=False), \
+                f"Gradient check failed for bias at layer {layer_i}.\n" \
+                f"Backprop: {all_dl_b[layer_i]}\n" \
+                f"Finite diff: {dl_bias}"
+
+    def test_nn_gradient_with_weight_change(self):
+        """Here we perform the same kind of check made in the notebook 7_2 to see if our derivatives are well calculated"""
+        # x = np.random.randn(1)
+        x = np.array([1.2])
+        target = np.array([1.0])
+        epsilon = 0.000001 # same delta used in notebook 7_2
+
+        prediction = self.nn(x)
+        print("LAYER",self.nn.layers[-1].h)
+        _, all_dl_w = self.nn._backward(prediction, target)
+
+        for layer_i in range(len(self.nn.layers)):
+            layer = self.nn.layers[layer_i]
+
+            weight_copy = np.array(layer.weights)
+            dl_w = np.zeros_like(all_dl_w[layer_i])
+
+            for row in range(layer.weights.shape[0]):
+                for col in range(layer.weights.shape[1]):
+                    # get current output
+                    output = self.nn(x)
+                    layer.weights[row][col]+=epsilon
+                    # get output with small change
+                    output_change = self.nn(x)
+
+                    #restore previous value
+                    layer.weights = weight_copy
+                    dl_w[row][col] = (self.loss_fn(output_change,target) - self.loss_fn(output, target))/epsilon
+
+                # assert np.allclose(all_dl_w[layer_i], dl_w, rtol=1e-5, atol=1e-8, equal_nan=False), \
+                    # f"Gradient check failed for weight at layer {layer_i}.\n" \
+                    # f"Backprop: {all_dl_w[layer_i]}\n" \
+                    # f"Finite diff: {dl_w}"
+
 